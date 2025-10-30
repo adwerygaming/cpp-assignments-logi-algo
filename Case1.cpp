4807c655823bda1bc994002150feb1c38f8f817c
MasDepan <50104357+adwerygaming@users.noreply.github.com>
2025-10-30 09:40:23 +0700

    Refactor: Improve student grading function and add comments
    - refactor(case1): Add grade parameter to GradeStudentAssignment function
    - chore(case1): Add comments explaining the benefits of using functions
    - refactor(case1): Declare and initialize variables in main function
    - chore(case1): Add comments for variable declaration in main

--- Case1.cpp
+++ Case1.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 // Membuat Fungsi Memberikan Nilai Kepada Mahasiswa
-// Dengan Nama Mata Kuliah, NPM Maha
+// Dengan Nama Mata Kuliah, NPM dan Nilai.
+
 int GradeStudentAssignment(string assignment, string student_npm, float grade) {
     if (assignment.empty() || student_npm.empty() || grade < 0) {
         cout << "Mohon untuk mencantumkan informasi assignment, student_npm dan grade. Semua harus lengkap." << endl;
@@ -14,10 +15,19 @@ int GradeStudentAssignment(string assignment, string student_npm, float grade) {
 }
 
 int main() {
+    // Deklarasi Variabel
     const string assignment = "Logika & Algoritma";
     const string student_npm = "25.12.3654";
     const float grade = 100;
 
+    // Menjalankan Fungsi
     GradeStudentAssignment(assignment, student_npm, grade);
     return 0;
 }
+
+// Kenapa menggunakan fungsi, dan tidak langsung di main?
+
+// Karena, jika memberikan nilai kepada 100 mahasiswa, kita harus meng-copy paste
+// 3 variabel dan 9 baris kode. Jika di total, akan menghasilkan 1,200 baris kode.
+// Salah satu fitur menggunakan fungsi adalah, jika ada perbaruan dalam memberikan nilai
+// kita tidak perlu untuk mengubah satu per satu untuk 100 mahasiswa. (effisien)
