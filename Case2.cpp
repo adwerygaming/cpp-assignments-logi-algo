0cce4b638a82813b773c06c88f801eeb450bbf5f
MasDepan <50104357+adwerygaming@users.noreply.github.com>
2025-10-30 10:00:51 +0700

    Add basic grading logic
    - feat(main): implement student grade evaluation
    - feat(main): define passing grade threshold

--- Case2.cpp
+++ Case2.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int main() {
-    
+    int kkm = 75;
+    float grade;
+
+    cout << "Masukkan Nilai Mahasiswa : ";
+    cin >> grade;
+
+    if (grade >= kkm) {
+        cout << "Mahasiswa dinyatakan lulus." << endl;
+    } else {
+        cout << "Mahasiswa dinyatakan tidak lulus." << endl;
+    }
+
     return 0;
 }
