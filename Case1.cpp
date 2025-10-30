2740778a6db346452170c12679fce26f1db0cf3b
MasDepan <50104357+adwerygaming@users.noreply.github.com>
2025-10-30 09:35:24 +0700

    Update function to correctly output assignment name
    - fix(Case1): Correctly output assignment name in GradeStudentAssignment
    - refactor(Case1): Declare main function variables as const

--- Case1.cpp
+++ Case1.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
 
+// Membuat Fungsi Memberikan Nilai Kepada Mahasiswa
+// Dengan Nama Mata Kuliah, NPM Maha
 int GradeStudentAssignment(string assignment, string student_npm, float grade) {
     if (assignment.empty() || student_npm.empty() || grade < 0) {
         cout << "Mohon untuk mencantumkan informasi assignment, student_npm dan grade. Semua harus lengkap." << endl;
         return 0;
     }
 
-    cout << "Memberikan nilai " << grade << " pada mahasiswa dengan NPM " << student_npm << " di tugas " + assignment << endl;
+    cout << "Memberikan nilai " << grade << " pada mahasiswa dengan NPM " << student_npm << " di tugas " << assignment << endl;
     return 0;
 }
 
 int main() {
-    string assignment = "Logika & Algoritma";
-    string student_npm = "25.12.3654";
-    float grade = 100;
+    const string assignment = "Logika & Algoritma";
+    const string student_npm = "25.12.3654";
+    const float grade = 100;
 
     GradeStudentAssignment(assignment, student_npm, grade);
     return 0;
